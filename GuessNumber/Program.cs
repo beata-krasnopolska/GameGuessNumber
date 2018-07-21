5a90253477d9483ef3e51dd5e780709633fe8324
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-07-21 08:42:29 +0200

    Add random number generator to all attempts

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,17 +10,14 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
-            //Ex 3 program generates a random number between 1 and 10, asks the user to guess the random nuber and than display the random number and a message indictaing whether the user's guess was to low, too high or correct
-
-
-            Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.Next(1, 10);
             string answer;
             int userNumber;
             int amountOfGuesses = 0;
 
             do
             {
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.Next(1, 10);
                 do {
                     Console.WriteLine("Guess the number between 1 and 10");
                     userNumber = int.Parse(Console.ReadLine());
@@ -40,7 +37,7 @@ namespace GuessNumber
                     }
                     amountOfGuesses++;
                 } while (userNumber != randomNumber);
-
+                
                 Console.WriteLine("You guessed {0} times", amountOfGuesses);
                 Console.WriteLine("Would you like to play again? :) Press \"T\" to start over or any other sign to quit");
                 answer = Console.ReadLine();
